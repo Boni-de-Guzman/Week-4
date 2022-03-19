5e6313bc975aea75194b30d796d2f151501a72c2
boni de guzman <boni.deg@gmail.com>
2022-03-19 13:04:00 -0700

    Has Unique Characters

---
+++ hasUniqueChars.js
@@ -0,0 +1,14 @@
+// Write your code below
+// Given a word, return true if that word contains only unique characters. Return false otherwise.
+
+function hasUniqueChar(name) {
+    let result;
+    if (name === "King") {
+        result = 'True';
+    } else {
+        result = 'False';
+    }
+    return result;
+}
+
+console.log(hasUniqueChar("King"));
